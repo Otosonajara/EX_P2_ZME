8b4ebf06cdbad65b28fa9b48f033b03edac7f0bf
Otosonajara <otosonajara@gmail.com>
2018-06-26 18:56:15 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/cesctm/ex_p2_ezm/MainActivity.kt
@@ -0,0 +1,24 @@
+package mx.edu.cesctm.ex_p2_ezm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+    var x = 1
+    var y = 2
+    var sqlquerry = ""
+
+
+
+
+
+}
+
+
